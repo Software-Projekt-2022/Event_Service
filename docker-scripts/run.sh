11edcb57edee40d098c842e9fcb8ab6ab9849475
Leon Stümpeley <leon.stuempeley@innovaitec.de>
2022-05-22 19:45:44 +0200

    extended sleeping time

--- docker-scripts/run.sh
+++ docker-scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Sleep 15 seconds for rabbitmq startup"
-sleep 15
+echo "Sleep 60 seconds for rabbitmq startup"
+sleep 60
 echo "Running Script"
 node rabbitmq-setup-script.js /default-settings.json
 echo "Finished"
