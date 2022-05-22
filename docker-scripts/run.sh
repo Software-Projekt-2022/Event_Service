81703d461a3c11e89e1ce35703b35f99d7a6c61a
Leon Stümpeley <leon.stuempeley@innovaitec.de>
2022-05-22 19:42:43 +0200

    Wait 15 seconds before runnig script

--- docker-scripts/run.sh
+++ docker-scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-echo test1
+echo "Sleep 15 seconds for rabbitmq startup"
+sleep 15
+echo "Running Script"
 node rabbitmq-setup-script.js /default-settings.json
-echo test3
+echo "Finished"
